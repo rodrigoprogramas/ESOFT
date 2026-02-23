5430fa660948dce62931151c510f99bd01055aae
Rodrigo Rodrigues <1231578@isep.ipp.pt>
2026-02-23 16:21:03 +0000

    Problem I imp 1/2

--- lab-classes-project/src/main/java/pt/ipp/isep/dei/esoft/labproject/problem/one/version/one/ProblemOne.java
+++ lab-classes-project/src/main/java/pt/ipp/isep/dei/esoft/labproject/problem/one/version/one/ProblemOne.java
@@ -1,15 +1,1 @@
-package pt.ipp.isep.dei.esoft.labproject.problem.one.version.one;
-
-public class ProblemOne {
-
-    /**
-     * Sort an array of integers in ascending order. Implementation (1/2).
-     *
-     * @param array Array of integers.
-     * @return Sorted array of integers.
-     */
-    public static int[] sortArrayAscending(int[] array) {
-        throw new UnsupportedOperationException(); //it means that the method is not implemented yet
-        //TODO: Students should implement this method
-    }
-}
+package pt.ipp.isep.dei.esoft.labproject.problem.one.version.one;public class ProblemOne {    /**     * Sort an array of integers in ascending order. Implementation (1/2).     *     * @param array Array of integers.     * @return Sorted array of integers.     */    public static int[] sortArrayAscending(int[] array) {        if(array != null){            for(int i = 0; i < array.length - 1; i++){                for(int j = 0 ; j < array.length - 1 - i ; j++){                    if(array[j] > array[j+1]){                        int temp = array[j];                        array[j] = array[j+1];                        array[j + 1 ]= temp;                    }                }            }        }        return array;    }}
